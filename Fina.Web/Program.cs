c25bbf86f98741e0219b45a22104e490ddc1d9e8
Guilhermano Silva <guilhermanodev@gmail.com>
2024-06-02 21:03:40 -0300

    Adiciona código do dia 3 - Web

--- Fina.Web/Program.cs
+++ Fina.Web/Program.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Fina.Web;
+using MudBlazor.Services;
+using Fina.Core;
+using Fina.Core.Handlers;
+using Fina.Web.Handlers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
+builder.Services.AddHttpClient(
+    WebConfiguration.HttpClientName,
+    opt =>
+    {
+        opt.BaseAddress = new Uri(Configuration.BackendUrl);
+    });
+
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
